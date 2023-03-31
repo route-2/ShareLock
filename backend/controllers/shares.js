2d9982fa46992ab821f3859116a68c70b561f213
route-2 <ruthurao@gmail.com>
2023-04-01 02:11:35 +0530

    feat:crypto

--- backend/controllers/shares.js
+++ backend/controllers/shares.js
@@ -1,59 +1,53 @@
 import User from "../model/user";
-const crypto = require('crypto');
-const Cryptr = require('cryptr');
+
+const Cryptr = require("cryptr");
 // postShares() we need mapping for acc => shares that they hold from split()
 // create model for mapping
 // getShares() fetch the shares in db
-// deleteShares() delete db instance 
+// deleteShares() delete db instance
 
 //normal get and post
-const getSharesByKey = async (req, res) => {
-  const cryptr = new Cryptr('KeyFromFE');
+export const getSharesByKey = async (req, res) => {
+  const cryptr = new Cryptr("KeyFromFE");
   //search By Key
-    const { shares } = req.params;
+  const { shares } = req.params;
   const { key } = req.body;
 
   try {
     // Find the user with the specified address
-    const user = await User.findOne({ address });
+    const user = await User.findOne({ shares });
 
     // decrypt the keys
-    const decipher = crypto.createDecipher('aes-256-cbc', key);
-    let decrypted = decipher.update(user.shares, 'hex', 'utf8');
-    decrypted += decipher.final('utf8');
+    const decryptedString = cryptr.decrypt(shares);
 
-    res.status(200).json({ shares: decrypted });
+    res.status(200).json({ shares: decryptedString });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Server error' });
+    res.status(500).json({ error: "Server error" });
   }
-    
-}
+};
 
-const postShares = async (req, res) => {
-    const { key, shares } = req.body;
-  
+export const postShares = async (req, res) => {
+  const { key, shares } = req.body;
 
   try {
     // Encrypt the user's shares using the specified key
-    const cipher = crypto.createCipher('aes-256-cbc', key);
-    let encrypted = cipher.update(shares, 'utf8', 'hex');
-    encrypted += cipher.final('hex');
+    const cryptr = new Cryptr(key);
+
+    const encryptedString = cryptr.encrypt(shares);
 
     // Create a new user with the specified address and encrypted shares
-    const user = new User({ address, shares: encrypted });
+    const user = new User({ address, shares: encryptedString });
     await user.save();
 
-    res.status(200).json({ message: 'Shares saved' });
+    res.status(200).json({ message: "Shares saved" });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Server error' });
+    res.status(500).json({ error: "Server error" });
   }
-
-   
-}
+};
 
 module.exports = {
-    getSharesByKey,
-    postShares
-}
+  getSharesByKey,
+  postShares,
+};
--- backend/routes/shares.js
+++ backend/routes/shares.js
@@ -1,7 +1,8 @@
+import {getSharesByKey,postShares} from "../controllers/shares.js"
+
 const express = require('express');
 const router = express.Router();
 const User = require('../model/user');
-import {getSharesByKey,postShares} from "../controllers/shares.js"
 
 router.get("/", getSharesByKey);
 router.post("/:id", postShares);
